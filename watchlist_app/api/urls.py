f6654ece30b56c7c68f5266e0cc7d5d99dbaf7ab
georgespicy <kolawolegeorge28743@gmail.com>
2023-02-15 12:25:36 +0100

    using serializers to GET an object and also GET all objects in a model

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from watchlist_app.api.views import movie_list, movie_details
+from django.urls import path 
+from watchlist_app.api.views import movie_list, movie_datails
 
 urlpatterns = [
     path('list/', movie_list, name="movie_list"),
-    path('<int:pk>', movie_details, name="movie_details")
+    path('<int:pk>', movie_datails, name="movie_details")
 ]
