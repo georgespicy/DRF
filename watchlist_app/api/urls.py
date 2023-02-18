f0776820811b9f65f555661e6fdd129e6ea34377
georgespicy <kolawolegeorge28743@gmail.com>
2023-02-18 23:47:18 +0100

    generic view making a post request with a get request

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path 
 # from watchlist_app.api.views import movie_list, movie_datails #fuction base url
-from watchlist_app.api.views import StreamListAV, StreamDetailAV, WatchlistAV, WatchDetailAV
+from watchlist_app.api.views import StreamListAV, StreamDetailAV, WatchlistAV, WatchDetailAV, ReviewList
 
 urlpatterns = [
     path('list/', WatchlistAV.as_view(), name='movie-list'),
     path('list/<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamListAV.as_view(), name='stream_list'),
     path('stream/<int:pk>/', StreamDetailAV.as_view(), name='stream-detail'),
-    # path('review/',  Review.as_view(), name='review'),
+    path('review/',  ReviewList.as_view(), name='review'),
     # path('list/', movie_list, name="movie_list"),
     # path('<int:pk>', movie_datails, name="movie_details")
 ]
