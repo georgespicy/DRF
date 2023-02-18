f2fd61c94775558461fd3775afbe57993231403e
georgespicy <kolawolegeorge28743@gmail.com>
2023-02-18 23:54:45 +0100

    generic view making a post request with a get request

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 # from watchlist_app.api.views import movie_list, movie_datails #fuction base url
-from watchlist_app.api.views import StreamListAV, StreamDetailAV, WatchlistAV, WatchDetailAV, ReviewList
+from watchlist_app.api.views import StreamListAV, StreamDetailAV, WatchlistAV, WatchDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchlistAV.as_view(), name='movie-list'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('stream/', StreamListAV.as_view(), name='stream_list'),
     path('stream/<int:pk>/', StreamDetailAV.as_view(), name='stream-detail'),
     path('review/',  ReviewList.as_view(), name='review'),
+    path('review/<int:pk>/',  ReviewDetail.as_view(), name='review_Detail'),
     # path('list/', movie_list, name="movie_list"),
     # path('<int:pk>', movie_datails, name="movie_details")
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -18,6 +18,13 @@ class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Generi
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+    
+class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)    
 
 class WatchlistAV(APIView):
     
